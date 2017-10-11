bb7ba8f8b033e98b6123b93b17a92ab5cdc87c85
Jari Saukkonen <jari.saukkonen@taika.fi>
2017-10-12 00:32:34 +0300

    Fix rebooting to bootloader

--- firmware/src/USB.cpp
+++ firmware/src/USB.cpp
@@ -156,7 +156,18 @@ void USB::handleCommands(uint8_t* data, unsigned int maxlen) {
                 break;
             }
             case REBOOT_BOOTLOADER: {
-                _reboot_Teensyduino_();
+                cli();
+                // disable watchdog, if enabled
+                // disable all peripherals
+                UDCON = 1;
+                USBCON = (1<<FRZCLK);  // disable USB
+                UCSR1B = 0;
+                delay(5);
+                EIMSK = 0; PCICR = 0; SPCR = 0; ACSR = 0; EECR = 0; ADCSRA = 0;
+                TIMSK0 = 0; TIMSK1 = 0; TIMSK3 = 0; TIMSK4 = 0; UCSR1B = 0; TWCR = 0;
+                DDRB = 0; DDRC = 0; DDRD = 0; DDRE = 0; DDRF = 0; TWCR = 0;
+                PORTB = 0; PORTC = 0; PORTD = 0; PORTE = 0; PORTF = 0;
+                asm volatile("jmp 0x7000");
                 break;
             }
             case SETRELAY: {
