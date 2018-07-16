4dffc186ce9d7c6c58e582e1f58b4418e1d15096
Jari Saukkonen <jari.saukkonen@taika.fi>
2018-07-16 23:36:12 +0300

    If KomaHub is already running, activate the already-running instance

--- software/KomaHub/KomaHub/Program.cs
+++ software/KomaHub/KomaHub/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,17 +10,49 @@ namespace KomaHub
 {
     static class Program
     {
+        [DllImport("user32.dll")] private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")] private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll")] private static extern bool IsIconic(IntPtr hWnd);
+
+        private const int SW_RESTORE = 9;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            if (showKomaHubWindowIfAlreadyRunning())
+            {
+                return;
+            }
+
             System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new KomaHubForm());
+        }
+
+        static bool showKomaHubWindowIfAlreadyRunning()
+        {
+            Process[] processes = Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName);
+            if (processes.Length > 1)
+            {
+                Process p = Process.GetCurrentProcess();
+                Process otherProcess = processes[0].Id == p.Id ? processes[0] : processes[1];
+                IntPtr hWnd = otherProcess.MainWindowHandle;
+                if (IsIconic(hWnd))
+                {
+                    ShowWindowAsync(hWnd, SW_RESTORE);
+                }
+                SetForegroundWindow(hWnd);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
